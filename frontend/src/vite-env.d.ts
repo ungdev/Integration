0cfb7dd19ee50c7c59ecaee10b09c79844208c92
Maxime Letoffet <maxime.letoffet@gmail.com>
2025-04-18 12:25:15 +0200

    Fix env

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,4 +1,4 @@
 /// <reference types="vite/client" />
-VITE_CAS_LOGIN_URL="https://cas.utt.fr/cas/login"
 VITE_SERVICE_URL="https://integration.utt.fr/"
+VITE_CAS_LOGIN_URL="https://cas.utt.fr/cas/login"
 VITE_API_URL="http://localhost:4001/api"

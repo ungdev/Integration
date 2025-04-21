1e2afd2fc4a6e9991fd5a60c4a68afc0408f0dd7
Maxime Letoffet <maxime.letoffet@gmail.com>
2025-04-21 17:37:59 +0200

    Infos section style + Team quotas

--- frontend/src/components/home/infosSection.tsx
+++ frontend/src/components/home/infosSection.tsx
@@ -53,29 +53,29 @@ export const Infos = () => {
       {/* Texte d'info */}
       <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="space-y-16">
-          <section className="text-center lg:text-left">
+          <section className="text-center lg:text-center">
             <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 text-blue-700">
               L'intégration, c'est quoi ?
             </h3>
-            <p className="text-base sm:text-lg leading-relaxed text-gray-700 max-w-3xl mx-auto lg:mx-0">
+            <p className="text-center sm:text-lg leading-relaxed text-gray-700 max-w-3xl mx-auto">
               C'est l'événement où l'ensemble des étudiants de l'UTT se mobilise pour concocter aux nouveaux (comme toi) une incroyable semaine durant laquelle tu découvriras la vie sur le campus, rencontreras une tonne de nouveaux amis et démarreras ta nouvelle vie d'étudiant ! Le tout se fait dans la bonne humeur et avec bienveillance, l'objectif c'est juste de s'amuser à fond !
             </p>
           </section>
 
-          <section className="text-center lg:text-left">
+          <section className="text-center lg:text-center">
             <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 text-blue-700">
               La petite histoire
             </h3>
-            <p className="text-base sm:text-lg leading-relaxed text-gray-700 max-w-3xl mx-auto lg:mx-0">
+            <p className="text-center sm:text-lg leading-relaxed text-gray-700 max-w-3xl mx-auto">
               Chaque année, deux factions composées d'une multitude d'équipes s'affrontent. Le thème de cette année : <strong>Vilains vs Justificers</strong>.
             </p>
           </section>
 
-          <section className="text-center lg:text-left">
+          <section className="text-center lg:text-center">
             <h3 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6 text-blue-700">
               Ce site, c'est quoi ?
             </h3>
-            <p className="text-base sm:text-lg leading-relaxed text-gray-700 max-w-3xl mx-auto lg:mx-0">
+            <p className="text-center sm:text-lg leading-relaxed text-gray-700 max-w-3xl mx-auto">
               C'est ici que tu trouveras toutes les informations nécessaires au déroulement de la semaine d'inté. Par exemple, tu pourras prendre ta place pour le WEI ou regarder quelle faction est la plus proche de la victoire.
             </p>
           </section>

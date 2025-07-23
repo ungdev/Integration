a3d61d789afb74b5406aa4490319bcc20e2d3093
Maxime Letoffet <maxime.letoffet@gmail.com>
2025-07-23 14:46:14 +0200

    teamRoutes for bot

--- backend/src/routes/team.routes.ts
+++ backend/src/routes/team.routes.ts
@@ -12,6 +12,7 @@ teamRouter.post("/user/create",checkRole("Student") ,teamController.createNewTea
 //Admin Routes
 teamRouter.post("/admin/createlight",checkRole("Admin") ,teamController.createNewTeamLight);
 teamRouter.get("/admin/teams",checkRole("Admin") ,teamController.getTeams);
+teamRouter.get("/admin/teamswithfactions",checkRole("Admin") ,teamController.getTeamsWithfactions);
 teamRouter.get("/admin/teamfaction",checkRole("Admin") ,teamController.getTeamFaction);
 teamRouter.put("/admin/modify",checkRole("Admin") ,teamController.modifyTeam);
 teamRouter.get("/admin/teamusers",checkRole("Admin") ,teamController.getTeamUsers);

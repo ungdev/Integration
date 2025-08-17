b8e1774a780ca9f7b45bd1fe976d0106ec2870a5
Arthur Dodin <dodinarthur@gmail.com>
2025-08-17 21:43:16 +0200

    update: add redirection text

--- frontend/src/pages/RoadbookRedirect.tsx
+++ frontend/src/pages/RoadbookRedirect.tsx
@@ -4,5 +4,11 @@ export const RoadbookRedirect: React.FC = () => {
     React.useEffect(() => {
         window.location.href = import.meta.env.VITE_ROADBOOK_URL;
     }, []);
-    return null;
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <div className="alert alert-info text-center" role="alert">
+                Redirection en cours...
+            </div>
+        </div>
+    );
 };

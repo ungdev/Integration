2635d494247baf4d0639d580914bf8cc43cb949d
Antoine <antoine.dufils@yahoo.fr>
2026-04-13 17:11:06 +0200

    feat: log all request with colors

--- backend/src/middlewares/request-logger.middleware.ts
+++ backend/src/middlewares/request-logger.middleware.ts
@@ -1,6 +1,17 @@
 import { Request } from 'express';
 import morgan from 'morgan';
 
+function getUserEmail(req: Request): string {
+  const user = req?.user;
+
+  if (!user || typeof user === 'string') {
+    return '-';
+  }
+
+  const maybeEmail = (user as Record<string, unknown>)['userEmail'];
+  return typeof maybeEmail === 'string' && maybeEmail.length > 0 ? maybeEmail : '-';
+}
+
 function getClientIp(req: Request): string {
   const forwardedFor = req.headers['x-forwarded-for'];
 
@@ -16,5 +27,21 @@ function getClientIp(req: Request): string {
 }
 
 morgan.token('client-ip', (req: Request) => getClientIp(req));
+morgan.token('user-email', (req: Request) => getUserEmail(req));
+morgan.token('statusColor', (req, res, args) => {
+    // get the status code if response written
+    var status = res.statusCode ?? undefined
 
-export const requestLogger = morgan(':method :url :status :response-time ms - :client-ip');
+    // get status color
+    var color = status >= 500 ? 31 // red
+        : status >= 400 ? 33 // yellow
+            : status >= 300 ? 36 // cyan
+                : status >= 200 ? 32 // green
+                    : 0; // no color
+
+    return '\x1b[' + color + 'm' + status + '\x1b[0m';
+});
+
+export const requestLogger = morgan(
+  '\x1b[33m:method :url :statusColor :response-time ms - :client-ip - :user-email',
+);

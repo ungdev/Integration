eceb1685d1740eb71df911084603aa789be38dbb
Arthur Dodin <dodinarthur@gmail.com>
2026-08-30 10:33:17 +0200

    fix: use Ok returns

--- backend/src/controllers/maker_battle.controller.ts
+++ backend/src/controllers/maker_battle.controller.ts
@@ -1,6 +1,6 @@
 import * as maker_battle_service from '../services/maker_battle.service';
 import type { AppRequestHandler } from '../types/http';
-import { Error } from '../shared/http/responses';
+import { Error, Ok } from '../shared/http/responses';
 import type { Group, GroupsList } from '../dto/maker_battle.dto';
 
 export const distributeGroups: AppRequestHandler<GroupsList> = async (req, res) => {
@@ -14,7 +14,9 @@ export const distributeGroups: AppRequestHandler<GroupsList> = async (req, res) 
             await maker_battle_service.distributeGroups(group);
         }
         await maker_battle_service.placeTeamsOnTables(groups);
-        return res.status(200).json({ success: true, message: 'Groups allocated successfully' });
+
+        Ok(res, { data: { success: true, message: 'Groups allocated successfully' } });
+        return;
     } catch (err) {
         return Error(res, { msg: 'Erreur lors de la distribution des groupes : ' + err });
     }
@@ -32,7 +34,8 @@ export const exportGroups: AppRequestHandler<Group> = async (req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Content-Disposition', `attachment; filename="maker_battle_${group}_${Date.now()}.json"`);
 
-        return res.status(200).json(exportData);
+        Ok(res, { data: exportData });
+        return;
     } catch (err) {
         return Error(res, { msg: "Erreur lors de l'exportation des groupes : " + err });
     }
@@ -48,9 +51,8 @@ export const getCurrentUser: AppRequestHandler = async (req, res) => {
 
         const userGroup = await maker_battle_service.getUserTeam(userId);
 
-        return res.status(200).json({
-            group: userGroup ?? null,
-        });
+        Ok(res, { data: { group: userGroup ?? null } });
+        return;
     } catch (err) {
         return Error(res, {
             msg: "Erreur lors de la récupération du groupe de l'utilisateur : " + err,
--- frontend/src/services/requests/maker_battle.service.ts
+++ frontend/src/services/requests/maker_battle.service.ts
@@ -3,15 +3,15 @@ import api from '../api';
 
 export const allocateGroups = async (groups: MakerBattleGroupTypeOption[]) => {
     const response = await api.post('/maker-battle/admin/allocate', { groups: groups.map((group) => group.value) });
-    return response.data;
+    return response.data.data;
 };
 
 export const fetchExportData = async (group: MakerBattleGroupTypeOption) => {
     const response = await api.get(`/maker-battle/admin/export/${group.value}`);
-    return response.data;
+    return response.data.data;
 };
 
 export const getUserGroup = async () => {
     const response = await api.get(`/maker-battle/group/me`);
-    return response.data;
+    return response.data.data;
 };
